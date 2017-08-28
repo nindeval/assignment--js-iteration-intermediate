a7cd1b9c8e11a2c4c6401aca70f787e7fa1941d7
travis <t3patterson@gmail.com>
2017-08-28 09:47:28 -0500

    updating with sumDigits

---
+++ ex-02-hasDoubleLetters.js
@@ -0,0 +1,14 @@
+
+/**
+ * hasDoubleLetters()
+ *
+ * Write a function called `hasDoubleLetters` that takes a string as
+ *
+ *
+**/
+
+
+console.assert( hasDoubleLetters('stutter') === true )
+console.assert( hasDoubleLetters('prospect') === false )
+console.assert( hasDoubleLetters('shoehorn') === false )
+console.assert( hasDoubleLetters('Aardvark') === true )
---
+++ ex-05-sumDigits.js
@@ -0,0 +1,20 @@
+
+/** ! Adventure Mode !
+ *
+ * sumDigits() 
+ *
+ * Write a function called `sumDigits` that accepts a number
+ * and returns a sum total of the value of the digits
+ *
+ *
+ *  Examples:
+ *    sumDigits(12) => 3
+ *    sumDigits(1112) => 5
+ *    sumDigits(406) => 10
+*/
+
+
+console.assert( sumDigits(123) === 6 )
+console.assert( sumDigits(911) === 11 )
+console.assert( sumDigits(8040) === 12)
+console.assert( sumDigits(173220) === 15 )
